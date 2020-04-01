a14901d51f07b8f646308fce34f364847e5ca3cd
valgrut <naruto987@seznam.cz>
2020-04-01 02:38:10 +0200

    First SUCCESSFULL build of text with ContentTable. Added some iamges, dataset and text parts

--- text/getFromMerlin.sh
+++ text/getFromMerlin.sh
@@ -9,3 +9,6 @@ fi
 
 #scp -v xpeska05@merlin.fit.vutbr.cz:~/Projekty/ITY/dokument.pdf .
 scp ${VERBOSE} ${LOGIN}@eva.fit.vutbr.cz:~/Bakalarka/projekt.pdf .
+# scp ${VERBOSE} ${LOGIN}@eva.fit.vutbr.cz:~/Bakalarka/projekt.out .
+# scp ${VERBOSE} ${LOGIN}@eva.fit.vutbr.cz:~/Bakalarka/projekt.ptc .
+# scp ${VERBOSE} ${LOGIN}@eva.fit.vutbr.cz:~/Bakalarka/projekt.toc .
--- text/sendToMerlin.sh
+++ text/sendToMerlin.sh
@@ -1,22 +1,26 @@
 #! /bin/bash
 
-FILES="projekt.tex Makefile"
 LOGIN="xpeska05"
 VERBOSE=""
 
+if [[ -f "bakalarka.zip" ]]; then
+    rm bakalarka.zip
+fi
+
+make clean
 zip -r bakalarka.zip *
-FILES="bakalarka.zip"
+ZIP="bakalarka.zip"
 
 if [ "$1" == "-v" ]; then
 	VERBOSE="-v"
 fi
 
 echo "Zkousim odeslat soubory pres server merlin."
-scp ${VERBOSE} ${FILES} ${LOGIN}@eva.fit.vutbr.cz:~/Bakalarka/
+scp ${VERBOSE} ${ZIP} ${LOGIN}@merlin.fit.vutbr.cz:~/Bakalarka/
 
 if [ $? == 1 ]; then
 	echo "Zkousim odeslat soubory pres server eva."
-	scp ${VERBOSE} ${FILES} ${LOGIN}@eva.fit.vutbr.cz:~/Bakalarka/
+	scp ${VERBOSE} ${ZIP} ${LOGIN}@eva.fit.vutbr.cz:~/Bakalarka/
 else
 	echo "Odeslani uspesne dokonceno."
 	exit
