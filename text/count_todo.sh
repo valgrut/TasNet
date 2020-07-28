85b49adca0d575aa8c2d3a55e69e6def2ae477b6
valgrut <naruto987@seznam.cz>
2020-07-28 02:31:32 +0200

    dir hiearchy adjustments

--- text/count_todo.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-HEAD=$(cat projekt-01-kapitoly-chapters.tex | nl | grep "Experimenty a vyhodnocení" | c1)
-cat projekt-01-kapitoly-chapters.tex | head -n $HEAD | grep todo | wc -l
